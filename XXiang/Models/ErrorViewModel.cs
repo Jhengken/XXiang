11e05673498530d2832891a317247b22850aab5e
JhengKen <hpu970390@gmail.com>
2023-02-03 15:35:48 +0800

    新增專案

---
+++ XXiang/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XXiang.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
